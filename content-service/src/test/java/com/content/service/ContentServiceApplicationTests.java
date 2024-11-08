c479a102727c6a46f369fd7770db71aac1af81a2
Soumyajeet Patra <ipunsoumya@gmail.com>
2024-11-08 13:18:13 +0530

    Initial commit

---
+++ content-service/src/test/java/com/content/service/ContentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.content.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
